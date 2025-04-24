7cb24b07fb8b36df1d51dc02da4b3b233466dfad
Fer992 <lf.giltr@gmail.com>
2025-04-23 19:56:11 -0500

    Modificacion de Login y Css Compras
    Se modifica el Login para colocar solo usuario y contraseña. Se cambian colores del modulo de Compras y Login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/Login.module.css';
+import Registro from '../Components/Registro';
 
 const Login = () => {
+  const [isRegistrese, setIsRegistrese] = useState(false);
+
+  const handleToggle = () => {
+    setIsRegistrese(!isRegistrese);
+  };
+
   return (
     <div className={styles.container}>
-      <h2>Hola! Inicia Sesión</h2>
-      <form className={styles.form}>
-        <input type="text" placeholder="Usuario" required />
-        <input type="password" placeholder="Contraseña" required />
-        <button type="submit">Ingresar</button>
-        <button type="submit">Registrate</button>
-      </form>
+      <h2>{isRegistrese ? 'Regístrate' : 'Hola! Inicia Sesión'}</h2>
+      {!isRegistrese ? (
+        <form className={styles.form}>
+      <input type="text" placeholder="Usuario" required />
+      <input type="password" placeholder="Contraseña" required />
+          <button type="submit">Ingresa</button>
+        </form>
+      ) : (
+        <Registro />
+      )}
+      <div className={styles.buttonContainer}>
+        <button onClick={handleToggle} className={styles.toggleButton}>
+          {isRegistrese ? 'Ya tengo una cuenta' : 'Regístrate'}
+        </button>
+      </div>
     </div>
   );
 };
